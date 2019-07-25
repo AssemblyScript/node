269f1c8c2c8d3f6ea2d113476f10e1f96de66a11
RedDwarfian <reddwarf86esb@gmail.com>
2019-07-24 20:52:51 -0400

    [Cleanup] [Optimize] Using recommended changes

--- assembly/node.d.ts
+++ assembly/node.d.ts
@@ -5,14 +5,14 @@ declare class Buffer extends Uint8Array {
   static allocUnsafe(size: i32): Buffer;
   /** This method asserts a value is a Buffer object via `value instanceof Buffer`. */
   static isBuffer<T>(value: T): bool;
-  /** Reads an unsigned integer at the designated offset. */
-  readUInt8(offset?: i32): u8;
-  /** Writes an inputted u8 value to the buffer, at the desired offset. */
-  writeUInt8(value:u8, offset?:i32): i32;
-  /** Writes an inputted value to the buffer, at the desired offset. */
-  writeInt8(value:i8, offset?:i32): i32;
   /** Reads a signed integer at the designated offset. */
   readInt8(offset?: i32): i8;
+  /** Reads an unsigned integer at the designated offset. */
+  readUInt8(offset?: i32): u8;
+  /** Writes an inputted value to the buffer, at the desired offset. */
+  writeInt8(value:i8, offset?:i32): i32;
+  /** Writes an inputted u8 value to the buffer, at the desired offset. */
+  writeUInt8(value:u8, offset?:i32): i32;
   /** Reads a signed 16-bit integer, stored in Little Endian format at the designated offset. */
   readInt16LE(offset?: i32): i16;
   /** Reads a signed 16-bit integer, stored in Big Endian format at the designated offset. */
