5f12802f69197c197f62808323b90ea23b1e6a82
Joshua Tenner <tenner.joshua@gmail.com>
2019-07-26 15:55:45 -0400

    [Implement] Multiple Parameter Callbacks with

--- assembly/events/index.ts
+++ assembly/events/index.ts
@@ -1,22 +1,22 @@
 import { BLOCK_OVERHEAD, BLOCK } from "rt/common";
 
-class Callback<T> {}
-class Dummy {}
 
 export class EventEmitter {
-  public static EventMap: Map<i32, Map<string, i32>> = new Map<i32, Map<string, i32>>();
-  public static registerEventCallback<T, U>(event: string): void {
+  public static EventMap: Map<i32, Map<string, u64>> = new Map<i32, Map<string, u64>>();
+  public static registerEventCallback<T, U>(event: string, length: u32 = 1): void {
     if (!isFunction<U>()) {
       ERROR("Cannot register event callback of type U where U is not a function.");
     }
     if (!EventEmitter.EventMap.has(idof<T>())) {
-      EventEmitter.EventMap.set(idof<T>(), new Map<string, i32>());
+      EventEmitter.EventMap.set(idof<T>(), new Map<string, u64>());
     }
     let ClassEvents = EventEmitter.EventMap.get(idof<T>());
     if (ClassEvents.has(event)) {
       throw new Error("EventMap already contains a definition for event: " + event);
     }
-    ClassEvents.set(event, idof<Callback<U>>());
+    let definition: u64 = <u64>idof<U>() | (<u64>length << 32);
+    ClassEvents.set(event, definition);
+    log<u64>(definition);
   }
 
   private _events: Map<string, u32[]> = new Map<string, u32[]>();
@@ -30,8 +30,9 @@ export class EventEmitter {
     if (!EventMap.has(rtId)) throw new Error("Cannot attach events to an EventEmitter with no EventMap definitions.");
     let ClassEvents = EventMap.get(rtId);
     if (!ClassEvents.has(event)) throw new Error("Event does not exist: " + event);
-    let cbId = idof<Callback<T>>();
-    let classEventCallbackID = ClassEvents.get(event);
+    let cbId = idof<T>();
+    let classEventSignature = ClassEvents.get(event);
+    let classEventCallbackID = <u32>(classEventSignature & 0xFFFFFFFF);
     assert(cbId == classEventCallbackID);
     if (!this._events.has(event)) this._events.set(event, new Array<u32>());
     let eventList = this._events.get(event);
@@ -39,12 +40,74 @@ export class EventEmitter {
     return this;
   }
 
-  public emit<T>(event: string, data: T): EventEmitter {
+  public emit<A = string | null, B = string | null, C = string | null, D = string | null, E = string | null, F = string | null, G = string | null, H = string | null, I = string | null, J = string | null>
+    (event: string, a: A = null, b: B = null, c: C = null, d: D = null, e: E = null, f: F = null, g: G = null, h: H = null, i: I = null, j: J = null): EventEmitter {
     if (this._events.has(event)) {
+      let rtId = changetype<BLOCK>(changetype<usize>(this) - BLOCK_OVERHEAD).rtId;
+      let ClassEvents = EventEmitter.EventMap.get(rtId);
+      assert(ClassEvents.has(event), "No event definition.");
+      let classEventSignature = ClassEvents.get(event);
+      let classEventArgLength = <u32>(classEventSignature >> 32) & 0xFFFFFFFF;
+      let classEventCallbackID = <u32>(classEventSignature & 0xFFFFFFFF);
       let list = this._events.get(event);
       let length = list.length;
-      for (let i: i32 = 0; i < length; i++) {
-        call_indirect(unchecked(list[i]), data);
+      switch (classEventArgLength) {
+        case 0: {
+          assert(classEventCallbackID == idof<() => void>());
+          for (let z = 0; z < length; z++) call_indirect(unchecked(list[z]));
+          break;
+        }
+        case 1: {
+          assert(classEventCallbackID == idof<(a: A) => void>());
+          for (let z = 0; z < length; z++) call_indirect(unchecked(list[z]), a);
+          break;
+        }
+        case 2: {
+          assert(classEventCallbackID == idof<(a: A, b: B) => void>());
+          for (let z = 0; z < length; z++) call_indirect(unchecked(list[z]), a, b);
+          break;
+        }
+        case 3: {
+          assert(classEventCallbackID == idof<(a: A, b: B, c: C) => void>());
+          for (let z = 0; z < length; z++) call_indirect(unchecked(list[z]), a, b, c);
+          break;
+        }
+        case 4: {
+          assert(classEventCallbackID == idof<(a: A, b: B, c: C, d: D) => void>());
+          for (let z = 0; z < length; z++) call_indirect(unchecked(list[z]), a, b, c, d);
+          break;
+        }
+        case 5: {
+          assert(classEventCallbackID == idof<(a: A, b: B, c: C, d: D, e: E) => void>());
+          for (let z = 0; z < length; z++) call_indirect(unchecked(list[z]), a, b, c, d, e);
+          break;
+        }
+        case 6: {
+          assert(classEventCallbackID == idof<(a: A, b: B, c: C, d: D, e: E, f: F) => void>());
+          for (let z = 0; z < length; z++) call_indirect(unchecked(list[z]), a, b, c, d, e, f);
+          break;
+        }
+        case 7: {
+          assert(classEventCallbackID == idof<(a: A, b: B, c: C, d: D, e: E, f: F, g: G) => void>());
+          for (let z = 0; z < length; z++) call_indirect(unchecked(list[z]), a, b, c, d, e, f, g);
+          break;
+        }
+        case 8: {
+          assert(classEventCallbackID == idof<(a: A, b: B, c: C, d: D, e: E, f: F, g: G, h: H) => void>());
+          for (let z = 0; z < length; z++) call_indirect(unchecked(list[z]), a, b, c, d, e, f, g, h);
+          break;
+        }
+        case 9: {
+          assert(classEventCallbackID == idof<(a: A, b: B, c: C, d: D, e: E, f: F, g: G, h: H, i: I) => void>());
+          for (let z = 0; z < length; z++) call_indirect(unchecked(list[z]), a, b, c, d, e, f, g, h, i);
+          break;
+        }
+        case 10: {
+          assert(classEventCallbackID == idof<(a: A, b: B, c: C, d: D, e: E, f: F, g: G, h: H, i: I, j: J) => void>());
+          for (let z = 0; z < length; z++) call_indirect(unchecked(list[z]), a, b, c, d, e, f, g, h, i, j);
+          break;
+        }
+        default: assert(false);
       }
     }
     return this;
--- tests/events.spec.ts
+++ tests/events.spec.ts
@@ -2,22 +2,38 @@ import { EventEmitter } from "events";
 
 let calls: i32 = 0;
 
-type i32Callback = (value: i32) => void;
-
+type i32Callback = (a: i32) => void;
+type i64Callback = (a: i64, b: i64) => void;
 class CustomEventEmitter extends EventEmitter {}
 
-EventEmitter.registerEventCallback<CustomEventEmitter, i32Callback>("data");
+EventEmitter.registerEventCallback<CustomEventEmitter, i32Callback>("data", 1);
+EventEmitter.registerEventCallback<CustomEventEmitter, i64Callback>("data2", 2);
 
 describe("events", () => {
   test("events", () => {
     let t = new CustomEventEmitter();
-    t.on<i32Callback>("data", <i32Callback>(value: i32) => {
+    calls = 0;
+    t.on<i32Callback>("data", (wrongName: i32) => {
       calls += 1;
-      expect<i32>(value).toBe(42);
+      expect<i32>(wrongName).toBe(42);
     });
     t.emit<i32>("data", 42);
     t.emit<i32>("data", 42);
     t.emit<i32>("data", 42);
+    expect<i32>(calls).toBe(3);
+  });
+
+  test("events2", () => {
+    let t = new CustomEventEmitter();
+    calls = 0;
+    t.on<i64Callback>("data2", (a: i64, b: i64) => {
+      calls += 1;
+      expect<i64>(a).toBe(42);
+      expect<i64>(b).toBe(42);
+    });
+    t.emit<i64, i64>("data2", <i64>42, <i64>42);
+    t.emit<i64, i64>("data2", <i64>42, <i64>42);
+    t.emit<i64, i64>("data2", <i64>42, <i64>42);
     expect<i32>(calls).toBe(3);
   });
 });
