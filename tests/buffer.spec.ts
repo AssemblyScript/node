a00145cf30933841d0536a4ef76b7bfab501916c
Joshua Tenner <tenner.joshua@gmail.com>
2019-08-06 12:02:23 -0400

    [test] use create convenience method instead

--- tests/buffer.spec.ts
+++ tests/buffer.spec.ts
@@ -143,8 +143,7 @@ describe("buffer", () => {
 
 
   test("#inspect", () => {
-    let buff = new Buffer(16);
-    for (let i = 0; i < 16; i++) buff[i] = i;
+    let buff = create<Buffer>([0x00, 0x01, 0x02, 0x03, 0x04, 0x05, 0x06, 0x07, 0x08, 0x09, 0x0a, 0x0b, 0x0c, 0x0d, 0x0e, 0x0f]);
     let result = buff.inspect();
     expect<string>(result).toBe("<Buffer 00 01 02 03 04 05 06 07 08 09 0a 0b 0c 0d 0e 0f>");
     INSPECT_MAX_BYTES = 5;
